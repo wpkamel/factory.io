182fc3b47ef71a83d421604ff73cf075b193e2d7
Kamel <127029906+wpkamel@users.noreply.github.com>
2023-03-13 20:47:24 +0100

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,21 @@
+
+jQuery( document ).ready( function ( $ ) {
+    $( "#back-top" ).hide();
+    $( function () {
+        $( window ).scroll( function () {
+            if ( $( this ).scrollTop() > 200 ) {
+                $( '#back-top' ).fadeIn();
+            } else {
+                $( '#back-top' ).fadeOut();
+            }
+        } );
+
+        // scroll body to 0px on click
+        $( '#back-top a' ).click( function () {
+            $( 'body,html' ).animate( {
+                scrollTop: 0
+            }, 800 );
+            return false;
+        } );
+    } );
+} );
